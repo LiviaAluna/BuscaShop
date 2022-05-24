ca3d9679bdfef6376f6f8f368a089c433c18c18f
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-24 15:20:42 -0300

    Arquivo de configuração do projeto.

---
+++ resources/config.php
@@ -0,0 +1,19 @@
+<?php
+/* Este arquivo especificas as configurações gerais do projeto, como credenciais
+ * de banco de dados. */
+
+$config = array(
+	// Banco de dados
+	"db" => array(
+		"dbname" => "buscashop",
+		"username" => "buscashop",
+		"password" => "prototype",
+		"host" => "localhost"
+	),
+
+	// Caminhos frequentes
+	// TODO
+);
+
+// EOF
+
