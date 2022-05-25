baa92a1dcda7b7c8435e5038c08f9eec4d09ab31
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-25 18:05:40 -0300

    Esta é a página principal.

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Prototype</title>
+		<link rel="stylesheet" href="css/default.css">
+	</head>
+
+	<body>
+		<!-- Cabeçalho -->
+		<?= require_once 'header.php'; ?>
+
+		<!-- Área principal -->
+		<div id="main-content">
+			<!-- A fazer -->	
+		</div>
+	</body>
+
+</html>
+
