6056b6976fe12b00897dbb3fafdda2f1ea3966d0
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-25 18:05:18 -0300

    Este é o cabeçalho reutilizável.

---
+++ public/header.php
@@ -0,0 +1,22 @@
+<div id="header">
+	<!-- Logo -->
+	<div id="logo-area"><h1>BuscaShop</h1></div>
+
+	<!-- Barra de pesquisa -->
+	<div id="search-area">
+		<form action="" method="POST">
+			<input id="filter-button" type="button" value="Filtro">
+			<input id="search-field" type="text" placeholder="produtos, lojas, preços...">
+			<input id="search-button" type="submit" value="Pesquisar">
+		</form>	
+	</div>
+
+	<!-- Opções -->
+	<!-- Estas opções devem mudar dependendo do estado do usuário -->
+	<div id="options-area">
+		<ul>
+			<li id="login-option"><a href="#">Entrar</a></li>
+			<li id="register-option"><a href="#">Cadastrar</a></li>
+		</ul>
+	</div>
+</div>
