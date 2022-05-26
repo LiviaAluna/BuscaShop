ea3834d9d53d132fe397e706b217444dcc90545a
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-26 16:04:27 -0300

    O rebeçalho reutilizável agora é identificado por 'resusable-header'. Alguns de seus componentes usam classes em vez de ids.

--- public/header.php
+++ public/header.php
@@ -1,19 +1,19 @@
-<div id="header">
+<div id="reusable-header">
 	<!-- Logo -->
-	<div id="logo-area"><h1>BuscaShop</h1></div>
+	<div class="logo-area"><h1>BuscaShop</h1></div>
 
 	<!-- Barra de pesquisa -->
-	<div id="search-area">
+	<div class="search-area">
 		<form action="" method="POST">
-			<input id="filter-button" type="button" value="Filtro">
-			<input id="search-field" type="text" placeholder="produtos, lojas, preços...">
-			<input id="search-button" type="submit" value="Pesquisar">
+			<input name="filter-button" type="button" value="Filtro">
+			<input name="search-field" type="text" placeholder="produtos, lojas, preços...">
+			<input type="submit" value="Pesquisar">
 		</form>	
 	</div>
 
 	<!-- Opções -->
 	<!-- Estas opções devem mudar dependendo do estado do usuário -->
-	<div id="options-area">
+	<div class="options-area">
 		<ul>
 			<li id="login-option"><a href="#">Entrar</a></li>
 			<li id="register-option"><a href="#">Cadastrar</a></li>
