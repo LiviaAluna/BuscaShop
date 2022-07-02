2a25051a0a73d0be92c4e8d0ef7b114d40594a99
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-07-01 22:16:42 -0300

    Estrutura da página inicial, cabeçalho e estilo.

---
+++ view/header.php
@@ -0,0 +1,22 @@
+<header id="main-header">
+
+	<div class="logo-area">
+		<h1 class="logo">BuscaShop</h1>
+	</div>
+
+	<div class="search-area">
+		<form action="" method="post">
+			<div class="search-field">
+				<button class="btn-settings"><i class="fa-solid fa-ellipsis"></i></button>
+				<input type="text" placeholder="Produtos, lojas...">
+				<button class="btn-search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+			</div>
+		</form>
+	</div>
+
+	<div class="menu-area">
+		<a href="#">Entrar</a>
+		<a href="#">Cadastrar</a>
+	</div>
+
+</header>
---
+++ view/index.php
@@ -0,0 +1,20 @@
+<!-- Página inicial do sistema BuscaShop -->
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta charset="utf-8">
+		<title>BuscaShop</title>
+		<link rel="stylesheet" href="css/default.css">
+		<script src="https://kit.fontawesome.com/0e44423ff5.js" crossorigin="anonymous"></script>
+	</head>
+
+	<body id="main-page">
+		<?php require_once 'header.php'; ?>
+		
+		<div id="main-container">
+			<!-- TODO -->
+		</div>
+	</body>
+
+</html>
